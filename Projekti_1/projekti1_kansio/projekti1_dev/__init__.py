39924cf1f5492bd6f30c87dff345bd11d3ce1ff5
waltterijok <Waltteri.Jokinen@student.lab.fi>
2026-04-27 23:09:58 +0300

    lisää siivousta

--- Projekti_1/projekti1_kansio/projekti1_dev/__init__.py
+++
@@ -1,4 +0,0 @@
-# db/__init__.py
-# models/__init__.py  
-# ui/__init__.py
-# (all empty for now — just marks the folder as a package)
--- Projekti_1/projekti1_kansio/projekti1_dev/config.py
+++
@@ -1,3 +0,0 @@
-DB_NAME = "Tasks.db"
-APP_NAME = "To_Do projekti"
-VERSION = "0.1.0"
